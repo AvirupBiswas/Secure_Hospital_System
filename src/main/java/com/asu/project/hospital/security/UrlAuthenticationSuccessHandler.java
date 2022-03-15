425994e001710d4ceb9bc9e91ba3f6bce564f53b
AvirupBiswas <avirupbiswas1992@gmail.com>
2022-03-14 19:09:15 -0700

    resolved merge conflict

--- src/main/java/com/asu/project/hospital/security/UrlAuthenticationSuccessHandler.java
+++ src/main/java/com/asu/project/hospital/security/UrlAuthenticationSuccessHandler.java
@@ -23,74 +23,69 @@ import org.springframework.security.web.authentication.AuthenticationSuccessHand
 import com.asu.project.hospital.entity.User;
 import com.asu.project.hospital.model.LoginSingleTon;
 import com.asu.project.hospital.repository.UserRepository;
-import com.asu.project.hospital.service.UserService;
 
 public class UrlAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
-
+	
 	@Autowired
 	private LoginSingleTon loginSingleTon;
-
-	@Autowired
-	private UserRepository userRepository;
-
-	@Autowired
-	private UserService userService;
 	
 	@Autowired
 	private UserServiceToCheckFailedLogin userServiceToCheckFailedLogin;
+	
+	@Autowired
+	private UserRepository userRepository;
 
-	protected Log logger = LogFactory.getLog(this.getClass());
+    protected Log logger= LogFactory.getLog(this.getClass());
 
-	private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+    private RedirectStrategy redirectStrategy=new DefaultRedirectStrategy();
 
-	@Override
-	public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse,
-			Authentication authentication) throws IOException {
-
-		String email = httpServletRequest.getParameter("username");
-		System.out.println("sucess email " + email);
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication)
+            throws IOException {
+    	
 		User user = userRepository.findByEmail(authentication.getName()).orElse(null);
 		if (user.getFailedAttempt() > 0) {
 			userServiceToCheckFailedLogin.resetFailedAttempts(user.getEmail());
 		}
-		handle(httpServletRequest, httpServletResponse, authentication);
-		clearAuthenticationAttributes(httpServletRequest);
-	}
+        handle(httpServletRequest, httpServletResponse, authentication);
+        clearAuthenticationAttributes(httpServletRequest);
+    }
 
-	protected void handle(HttpServletRequest request, HttpServletResponse response, Authentication authentication)
-			throws IOException {
+    protected void handle(HttpServletRequest request, HttpServletResponse response, Authentication authentication)
+            throws IOException{
 
-		String targetUrl = determineTargetUrl(authentication);
+        String targetUrl = determineTargetUrl(authentication);
 
-		if (response.isCommitted()) {
-			logger.debug("Response is already committed. Unable to redirect to" + targetUrl);
-			return;
-		}
-		redirectStrategy.sendRedirect(request, response, targetUrl);
-	}
+        if(response.isCommitted()){
+            logger.debug("Response is already committed. Unable to redirect to" + targetUrl);
+            return;
+        }
+        redirectStrategy.sendRedirect(request,response,targetUrl);
+    }
 
-	protected String determineTargetUrl(final Authentication authentication) {
+    protected String determineTargetUrl(final Authentication authentication){
+    	
+    	loginSingleTon.setTimestamp(new Date());
+        Map<String, String> roleTargetUrlMap = new HashMap<>();
+        roleTargetUrlMap.put("ADMIN", "/admin/home");
+        roleTargetUrlMap.put("PATIENT", "/patient/home");
 
-		loginSingleTon.setTimestamp(new Date());
-		Map<String, String> roleTargetUrlMap = new HashMap<>();
-		roleTargetUrlMap.put("ADMIN", "/admin/home");
+        final Collection<? extends GrantedAuthority> authorities=authentication.getAuthorities();
 
-		final Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        for(final GrantedAuthority grantedAuthority : authorities){
+            String authorityName=grantedAuthority.getAuthority();
+            if(roleTargetUrlMap.containsKey(authorityName)){
+                return roleTargetUrlMap.get(authorityName);
+            }
+        }
+        throw new IllegalStateException();
+    }
 
-		for (final GrantedAuthority grantedAuthority : authorities) {
-			String authorityName = grantedAuthority.getAuthority();
-			if (roleTargetUrlMap.containsKey(authorityName)) {
-				return roleTargetUrlMap.get(authorityName);
-			}
-		}
-		throw new IllegalStateException();
-	}
-
-	protected void clearAuthenticationAttributes(HttpServletRequest request) {
-		HttpSession httpSession = request.getSession(false);
-		if (httpSession == null) {
-			return;
-		}
-		httpSession.removeAttribute(WebAttributes.AUTHENTICATION_EXCEPTION);
-	}
+    protected void clearAuthenticationAttributes(HttpServletRequest request){
+        HttpSession httpSession=request.getSession(false);
+        if(httpSession==null){
+            return;
+        }
+        httpSession.removeAttribute(WebAttributes.AUTHENTICATION_EXCEPTION);
+    }
 }
